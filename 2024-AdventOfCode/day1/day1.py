570090e82878dd20a260c5f02b84013c702d606b
kyle.spicer <kyle.d.spicer.mil@army.mil>
2024-11-30 22:48:30 -0500

    Update pre-commit and move .gitignore into proper repo.

--- 2024-AdventOfCode/day1/day1.py
+++ 2024-AdventOfCode/day1/day1.py
@@ -1,12 +1,15 @@
+# Advent of Code: 2024, Day 1
 
 SAMPLE_FILE = "sample.txt"
 DATA_FILE = "data.txt"
+
 
 def main():
     with open(SAMPLE_FILE, "r") as file:
         data = file.read()
 
     print(f"{data = }")
-    
-if __name__=="__main__":
+
+
+if __name__ == "__main__":
     main()
