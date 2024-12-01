96f2df8a488987adc46190b82745600cdf2f7da9
kyle.spicer <kyle.d.spicer.mil@army.mil>
2024-12-01 09:22:05 -0500

    Add day1 solution

--- 2024-AdventOfCode/day1/day1.py
+++ 2024-AdventOfCode/day1/day1.py
@@ -4,11 +4,41 @@ SAMPLE_FILE = "sample.txt"
 DATA_FILE = "data.txt"
 
 
-def main():
-    with open(SAMPLE_FILE, "r") as file:
-        data = file.read()
+def part_one(l_list, r_list) -> int:
+    total_dist = 0
 
-    print(f"{data = }")
+    l_sorted = sorted(l_list)
+    r_sorted = sorted(r_list)
+
+    for i in range(len(l_sorted)):
+        total_dist += abs(l_sorted[i] - r_sorted[i])
+
+    return total_dist
+
+
+def part_two(l_list, r_list):
+    total_similarity = 0
+
+    for num in l_list:
+        count = r_list.count(num)
+        total_similarity += count * num
+
+    return total_similarity
+
+
+def main():
+    with open(DATA_FILE, "r") as file:
+        data = [list(map(int, line.split())) for line in file]
+
+    l_list, r_list = zip(*data)
+    l_list = list(l_list)
+    r_list = list(r_list)
+
+    part_one_answer = part_one(l_list.copy(), r_list.copy())
+    print(f"Part One: {part_one_answer}")
+
+    part_two_answer = part_two(l_list.copy(), r_list.copy())
+    print(f"Part Two: {part_two_answer}")
 
 
 if __name__ == "__main__":
